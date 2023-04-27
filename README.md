4efc4f423e944e9db1d1523337779e2d6f2d62c3
Cybercubic <investologist2020@icloud.com>
2023-04-27 12:37:58 +0000

    27.04.A

---
+++ README.md
@@ -0,0 +1,17 @@
+# Candle-Handle-Scaled 1.0.0
+
+1. The tool for search of good assets to buy/sell, which considers ONLY historical prices. It should find **good entry points**, not good investments.
+
+2. This tool uses **Financial Modeling Prep API**. You need your own FMP API key to use CH Scaled.
+
+3. You can use CH Scaled both for US stocks or crypto, doesn't matter. However, FMP API is made mainly for stock market, data for crypto there has lower quality.
+
+4. CH Scaled shows final score + ticker as output. This final score evaluates how good asset is to consider to long-term buy or sell. Also it prints some useful data. Currently you can choose what to print only by changing the code of our tool (but it should be easy).
+
+5. Scaled version makes it's search out of **ticker list**. That list may be really large, but we prefer lists of about 100 hundred assets (3 of them are in "tickers" file). You are free to change ticker lists anyhow.
+
+6. Candle-Handle-Scaled may be improved by fine-tune of it's parameters. It should be a great area to work on. We are also going to add some new features later.
+
+7. **Don't make any trading decisions considering only evaluations of this platform.** It WON'T make you rich. It was made only to help traders in their work as a useful tool.
+
+8. *Be careful on financial markets. All losses there is your own responsibility!*
---
+++ main.py
@@ -0,0 +1,54 @@
+﻿import dataloadKuCoin as dataload
+import funcs as funcs
+from tickers import tickers_crypto_KuCoin
+
+#main function
+
+def Candle_Search (ticker):
+    
+    open_list, high_list, low_list, close_list = dataload.Load_HP(ticker)[0], dataload.Load_HP(ticker)[1], dataload.Load_HP(ticker)[2], dataload.Load_HP(ticker)[3]
+    
+    short_list = close_list[210:260]
+    print(short_list)
+    skeleton1 = funcs.skeleton_founder(short_list, 1, 50, 8)
+    print(skeleton1)
+    likelihood_dict1 = funcs.find_nature(skeleton1)[2]
+    print(likelihood_dict1)
+    
+    short_list = close_list[246:260]
+    skeleton2 = funcs.skeleton_founder(short_list, 1, 14, 2)
+    likelihood_dict2 = funcs.find_nature(skeleton2)[2]
+    print(likelihood_dict2)
+    
+    my_array = funcs.pl_preprocessing(open_list, high_list, low_list, close_list)
+    
+    isto_array = funcs.make_isto(my_array)
+    
+    emotion_array = funcs.final_isto(isto_array)
+    
+    k = funcs.calculate_k(likelihood_dict1, likelihood_dict2, emotion_array)
+    
+    result = [k, ticker]
+    
+    return result
+
+ticker_list = tickers_crypto_KuCoin
+
+top_dict = {}
+
+for ticker in ticker_list:
+    try:
+        a = Candle_Search(ticker)
+        print(a)
+        
+        dict_score = a[0]
+        dict_ticker = a[1]
+        
+        top_dict[dict_ticker] = round(dict_score, 2)
+    
+    except Exception:
+        print(ticker, '- NOPE')
+        
+top_dict = sorted(top_dict.items(), key=lambda x:x[1], reverse=True)
+
+print(top_dict)
